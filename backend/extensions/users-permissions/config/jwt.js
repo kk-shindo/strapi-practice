beda9d31148d9d601c0536a5a65aed78135bca57
kk-shindo <kk-shindo@respect-pal.jp>
2020-06-23 17:24:47 +0900

    add backend files

---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2c2be24b-a8f4-4543-8a93-0c8215cdae9e'
+};
---
+++ backend/public/mail.php
@@ -0,0 +1,3 @@
+<?php
+header('Content-type: application/json');
+echo json_encode('送信が完了しました！');
